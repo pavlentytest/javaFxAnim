93dbb27211dd252d392073d459456a24f0b785f4
pavlentytest <impetus@yandex.ru>
2019-03-16 10:31:32 +0300

    Sprite animation

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,47 +10,56 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
-	@Override
-	public void start(Stage ps) {
-		ps.setTitle("Animation example");
-		Group root = new Group();
-		Scene theScene = new Scene(root);
-		ps.setScene(theScene);
-		
-		Canvas canvas = new Canvas(512,512);
-		root.getChildren().add(canvas);
-		
-		Image earth = new Image("earth.png");
-		Image sun = new Image("sun.png");
-		Image space = new Image("space.png");
-		
-		
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-		
-		final long startNanoTime = System.nanoTime();
-		System.out.println(startNanoTime);
-		
-		new AnimationTimer() {
+	 public static void main(String[] args) 
+	    {
+	        launch(args);
+	    }
 
-			@Override
-			public void handle(long t) {
-				double diff = (t - startNanoTime) / 1000000000.0;
-				//System.out.println(diff);
-				double x = 230 + 128 * Math.cos(diff);
-				double y = 230 + 128 * Math.sin(diff);
-				gc.drawImage(space,0,0);
-				gc.drawImage(earth,x,y);
-				gc.drawImage(sun, 196, 196);
-			}
-			
-		}.start();
-		ps.show();
-		
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+	    @Override
+	    public void start(Stage theStage) 
+	    {
+	        theStage.setTitle( "AnimatedImage Example" );
+
+	        Group root = new Group();
+	        Scene theScene = new Scene( root );
+	        theStage.setScene( theScene );
+
+	        Canvas canvas = new Canvas( 512, 512 );
+	        root.getChildren().add( canvas );
+
+	        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+	        Image earth = new Image( "earth.png" );
+	        Image sun   = new Image( "sun.png" );
+	        Image space = new Image( "space.png" );
+
+	        AnimatedImage ufo = new AnimatedImage();
+	        Image[] imageArray = new Image[6];
+	        for(int i=0;i<6;i++) {
+	        	imageArray[i] = new Image("ufo_"+i+".png");
+	        }
+	        ufo.frames = imageArray;
+	        ufo.duration = 1;
+
+	        final long startNanoTime = System.nanoTime();
+
+	        new AnimationTimer()
+	        {
+	            public void handle(long currentNanoTime)
+	            {
+	                double t = (currentNanoTime - startNanoTime) / 1000000000.0; 
+
+	                double x = 232 + 128 * Math.cos(t);
+	                double y = 232 + 128 * Math.sin(t);
+
+	                gc.drawImage( space, 0, 0 );
+	                gc.drawImage( earth, x, y );
+	                gc.drawImage( sun, 196, 196 );
+	                gc.drawImage(ufo.getFrame(t), 450, 40);
+	            }
+	        }.start();
+	        
+	        theStage.show();
+	    }
 }
